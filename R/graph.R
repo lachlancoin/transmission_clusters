c3ccb5cc6a0c29bfc80e9c6a2985b59e168558c5
lachlancoin <ljmcoin@gmail.com>
2023-07-06 12:51:41 +1000

    update

--- R/graph.R
+++ R/graph.R
@@ -4,6 +4,7 @@ libdir=getOption("fspls.libdir","C:/Users/LCOIN/R-4.0.2/library")
 
 library(ggraph)
 library(tidygraph)
+library(readxl)
 
 ##FUNCTIONS
 
@@ -12,24 +13,38 @@ library(tidygraph)
 }
 
 .readInputFile<-function(input_file){
-  matr = read.delim(input_file ,head=F,sep=",")
-  rownames(matr) = matr[,1]
-  vertices = data.frame(matr[-1,1,drop=F] )
-  colnames(matr) = matr[1,]
-  matr = apply(matr[-1,-1],c(1,2),as.numeric)
-  names(vertices) = "name"
-  vertices = cbind(vertices, t(data.frame(apply(vertices, 1,strsplit,"_"))))
-  names(vertices)[4:5]=c("year","type")
-  vertices$year =as.numeric(as.character(vertices$year))
-  vertices$type = factor(vertices$type)
-  list(matr = matr, vertices = vertices)
+  matr = read_xlsx(input_file, sheet="distance_matrix")
+  #matr = read.delim(input_file ,head=F,sep=",")
+  #rownames(matr) = matr[,1]
+  vertices = read_xlsx(input_file, sheet="metadata")
+  vertices = vertices[!is.na(vertices[,1]),,drop=F]
+  mtch = read_xlsx(input_file, sheet="match")
+  thresholds = read_xlsx(input_file, sheet="thresholds")
+  
+  for(k in 1:nrow(mtch)){
+    if(mtch$Type[[k]]=="numeric") vertices[[mtch$Key[[k]]]]=as.numeric(vertices[[mtch$Key[[k]]]])
+    if(mtch$Type[[k]]=="categorical") vertices[[mtch$Key[[k]]]]=as.factor(vertices[[mtch$Key[[k]]]])
+    
+  }
+
+#  rownames(vertices) = vertices[,1]
+  
+ 
+ # colnames(matr) = matr[1,]
+  matr= apply(as.matrix(matr[,-1]),c(1,2), as.numeric)
+  rownames(matr) = colnames(matr)
+  list(matr =matr, vertices = vertices, match = mtch, thresholds = thresholds)
 }
 
 
-.getEdges<-function(matr,vertices, thresh){
+.getEdges<-function(matr, thresh){
   print(thresh)
+  tv = paste(thresh, collapse=" ")
+  print(tv)
+  fn = eval(str2lang(paste("function(x) x", thresh[[1]], thresh[[2]]))) #type))
+  
   edges=apply(matr,1,function(v) {
-    v[which(v>=thresh)]
+    v[which(fn(v))]
   })
   edges = edges[unlist(lapply(edges, length))>0]
   if(length(edges)==0){
@@ -41,44 +56,57 @@ library(tidygraph)
     names(edgek)=lapply(edgek,function(v) paste(v[1:2], collapse="_"))
     edgek[unlist(lapply(edgek, function(v)v[1]!=v[2]))]
   }), rec=F)
-  edges2= data.frame(cbind(t(data.frame(edges1[!duplicated(names(edges1))])), thresh))
   }
+  edges2= data.frame(cbind(t(data.frame(edges1[!duplicated(names(edges1))])), tv))
   names(edges2) =c("from","to","dist","thresh")
   edges2$dist = as.numeric(edges2$dist)
-  edges2$thresh = as.numeric(edges2$thresh)
   edges3 = .removeDupl(edges2)
   edges3
 }
 
-.plotNetwork<-function(edges3, vertices, showlabel=F){
+.plotNetwork<-function(edges3, vertices, mtch, ncol = 2){
+  color = subset(mtch, Value=="color")
+  shape = subset(mtch, Value=="shape")
+  label = subset(mtch, Value=="label")
+  
   g1 <- igraph::graph_from_data_frame(edges3, directed=FALSE, vertices=vertices)
   gg = ggraph(g1, layout = 'kk') +
     #geom_edge_fan()+
-    geom_edge_fan(aes(colour = dist)) + 
-    geom_node_point(size=3, aes( shape=type,color=year)) +
+    geom_edge_fan(aes(color = dist)) + 
+    geom_node_point(size=3, aes_string( shape=shape$Key,color=color$Key)) +
     #scale_color_brewer(type="qual", palette=3) +
     theme_graph(foreground = 'steelblue', fg_text_colour = 'white', base_size = 14) +
-      facet_wrap(~thresh, scales = "free", ncol = 2) +
+      facet_wrap(~thresh, scales = "free", ncol = ncol) +
     scale_edge_colour_gradientn(colours = c('#313695', '#ffffbf', '#a50026'))
-  if(showlabel)gg<-gg+ geom_node_label(aes(label = name),  
+  gg<-gg+ geom_node_label(aes_string(label = label$Key),  
                                        repel = TRUE, show.legend = FALSE, family = "serif")
   gg
 }
-
-
-
-input_file= "../../SC2-care-homes-anonymised/processed_data/alignment_carehome_20200720_snp_dist.txt"
-input_file = "../20230705_HCVExampleData_PercentID.csv"
-inp =.readInputFile(input_file)
-matr = inp$matr
-vertices = inp$vertices
-thresholds = seq(80, 95,5)
-edges3 = .getEdges(matr, vertices, thresholds[[1]])
-for(k in 2:length(thresholds)){
-  edges3 = rbind(edges3, .getEdges(matr, vertices, thresholds[[k]]))
+.merge<-function(edges_all){
+  edges3 = edges_all[[1]]
+  for(k in 2:length(edges_all)){
+    edges3 =rbind(edges3,edges_all[[k]])
+  }
+  edges3
 }
-gg<-.plotNetwork(edges3,vertices, FALSE)
-  #edges3$'dist'=as.numeric(edges3$dist)
-ggsave("network.pdf", width = 9, height = 12, device = cairo_pdf)
 
+.runAll<-function(input_file, output_pdf = "network.pdf", multiplier=15){
+ 
+  
+    inp =.readInputFile(input_file)
+    matr = inp$matr
+    vertices = inp$vertices
+    thresholds =inp$thresholds
+    edges3 =.merge(apply(thresholds,1, function(t) .getEdges(matr,  t)))
+    ncol = 3
+    nrow = ceiling(length(thresholds)/3)
+    gg<-.plotNetwork(edges3,vertices, inp$match,ncol=ncol)
+      #edges3$'dist'=as.numeric(edges3$dist)
+    ggsave(output_pdf, width = ncol * multiplier, height = nrow * multiplier, units="cm", device = cairo_pdf)
+    invisible(gg)
+}
+##THIS IS BIT THAT RUNS CODE 
+#source("graph.R")
+input_file="../20230309_ExampleMetadata.xlsx"
+.runAll(input_file, output_pdf="network1.pdf")
 
